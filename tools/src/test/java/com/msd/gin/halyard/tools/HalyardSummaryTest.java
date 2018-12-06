0e71e769e8755d9a318a308512cb6cfadad13ada
Adam Sotona <adam.sotona@merck.com>
2018-12-06 17:40:53 +0100

    more Halyard Summary tests

--- tools/src/test/java/com/msd/gin/halyard/tools/HalyardSummaryTest.java
+++ tools/src/test/java/com/msd/gin/halyard/tools/HalyardSummaryTest.java
@@ -65,12 +65,13 @@ public class HalyardSummaryTest {
         IRI predicates[] = generateIRIs(4, "http://whatever/predicate#");
         IRI literalTypes[] = new IRI[]{XMLSchema.INT, XMLSchema.STRING, XMLSchema.DOUBLE};
         IRI instances[] = generateIRIs(50, "http://whatever/instance#");
-        Literal literals[] = new Literal[1];
+        Literal literals[] = new Literal[10];
         SimpleValueFactory svf = SimpleValueFactory.getInstance();
         Random r = new Random(66);
         Map<IRI, Collection<IRI>> classMap = new HashMap<>();
         Map<IRI, Integer> classCardinalities = new HashMap<>(), predicateCardinalities = new HashMap<>();
         Map<List<IRI>, Integer> domainCardinalities = new HashMap<>(), rangeCardinalities = new HashMap<>(), domainAndRangeCardinalities = new HashMap<>();
+        Map<List<IRI>, Integer> rangeTypeCardinalities = new HashMap<>(), domainAndRangeTypeCardinalities = new HashMap<>(), classClassCardinalities = new HashMap<>();
         for (int i = 0; i < literals.length; i++) {
             literals[i] = svf.createLiteral("0", literalTypes[r.nextInt(literalTypes.length)]);
         }
@@ -81,6 +82,12 @@ public class HalyardSummaryTest {
                 for (IRI clazz : classes) {
                     if (r.nextBoolean()) {
                         sail.addStatement(instance, RDF.TYPE, clazz);
+                        for (IRI otherClass : clsC) {
+                            List<IRI> keys = new ArrayList<>(2);
+                            keys.add(clazz);
+                            keys.add(otherClass);
+                            classClassCardinalities.put(keys, classClassCardinalities.getOrDefault(keys, 0) + 1);
+                        }
                         clsC.add(clazz);
                         classCardinalities.put(clazz, classCardinalities.getOrDefault(clazz, 0) + 1);
                     }
@@ -98,7 +105,16 @@ public class HalyardSummaryTest {
                             keys.add(predicate);
                             keys.add(domainClass);
                             domainCardinalities.put(keys, domainCardinalities.getOrDefault(keys, 0) + 1);
+                            keys = new ArrayList<>(3);
+                            keys.add(predicate);
+                            keys.add(domainClass);
+                            keys.add(l.getDatatype());
+                            domainAndRangeTypeCardinalities.put(keys, domainAndRangeTypeCardinalities.getOrDefault(keys, 0) + 1);
                         }
+                        List<IRI> keys = new ArrayList<>(2);
+                        keys.add(predicate);
+                        keys.add(l.getDatatype());
+                        rangeTypeCardinalities.put(keys, rangeTypeCardinalities.getOrDefault(keys, 0) + 1);
                     }
                     if (r.nextBoolean()) {
                         IRI otherInstance = instances[r.nextInt(instances.length)];
@@ -112,7 +128,7 @@ public class HalyardSummaryTest {
                         }
                         for (IRI rangeClass: classMap.getOrDefault(otherInstance, Collections.emptySet())) {
                             for (IRI domainClass: classMap.getOrDefault(instance, Collections.emptySet())) {
-                                List<IRI> keys = new ArrayList<>(2);
+                                List<IRI> keys = new ArrayList<>(3);
                                 keys.add(predicate);
                                 keys.add(domainClass);
                                 keys.add(rangeClass);
@@ -155,6 +171,15 @@ public class HalyardSummaryTest {
             }
             for (Map.Entry<List<IRI>, Integer> me : domainAndRangeCardinalities.entrySet()) {
                 assertCardinality(null, HalyardSummary.ReportType.DomainAndRangeCardinality.IRI, me.getValue(), model, HalyardSummary.PREDICATE, me.getKey().get(0), HalyardSummary.DOMAIN, me.getKey().get(1), HalyardSummary.RANGE, me.getKey().get(2));
+            }
+            for (Map.Entry<List<IRI>, Integer> me : rangeTypeCardinalities.entrySet()) {
+                assertCardinality(null, HalyardSummary.ReportType.RangeTypeCardinality.IRI, me.getValue(), model, HalyardSummary.PREDICATE, me.getKey().get(0), HalyardSummary.RANGE_TYPE, me.getKey().get(1));
+            }
+            for (Map.Entry<List<IRI>, Integer> me : domainAndRangeTypeCardinalities.entrySet()) {
+                assertCardinality(null, HalyardSummary.ReportType.DomainAndRangeTypeCardinality.IRI, me.getValue(), model, HalyardSummary.PREDICATE, me.getKey().get(0), HalyardSummary.DOMAIN, me.getKey().get(1), HalyardSummary.RANGE_TYPE, me.getKey().get(2));
+            }
+            for (Map.Entry<List<IRI>, Integer> me : classClassCardinalities.entrySet()) {
+                assertCardinality(null, HalyardSummary.ReportType.ClassesOverlapCardinality.IRI, me.getValue(), model, HalyardSummary.CLASS, me.getKey().get(0), HalyardSummary.CLASS, me.getKey().get(1));
             }
         }
     }
