dd6a475f8b58acf6c383f61b20338ade2031c96f
Mark Hale <mark.hale@physics.org>
2019-06-07 12:50:32 +0100

    Test fixes.

--- sail/src/test/java/com/msd/gin/halyard/sail/HBaseSailTest.java
+++ sail/src/test/java/com/msd/gin/halyard/sail/HBaseSailTest.java
@@ -539,7 +539,7 @@ public class HBaseSailTest {
         assertEquals(100.0, sail.statistics.getCardinality(q1), 0.01);
         assertEquals(100.0, sail.statistics.getCardinality(q2), 0.01);
         assertEquals(100.0, sail.statistics.getCardinality(q3), 0.01);
-        assertEquals(1.0, sail.statistics.getCardinality(q4), 0.01);
+        assertEquals(0.0001, sail.statistics.getCardinality(q4), 0.00001);
 		try (SailConnection conn = sail.getConnection()) {
 	        conn.addStatement(HALYARD.STATS_ROOT_NODE, VOID.TRIPLES, f.createLiteral(10000l), HALYARD.STATS_GRAPH_CONTEXT);
 			conn.addStatement(f.createIRI(HALYARD.STATS_ROOT_NODE.stringValue() + "_property_" + HalyardTableUtils.encode(HalyardTableUtils.id(RDF.TYPE))), VOID.TRIPLES, f.createLiteral(5000l), HALYARD.STATS_GRAPH_CONTEXT);
@@ -549,7 +549,7 @@ public class HBaseSailTest {
         assertEquals(5000.0, sail.statistics.getCardinality(q1), 0.01);
         assertEquals(20.0, sail.statistics.getCardinality(q2), 0.01);
         assertEquals(100.0, sail.statistics.getCardinality(q3), 0.01);
-        assertEquals(1.0, sail.statistics.getCardinality(q4), 0.01);
+        assertEquals(0.0001, sail.statistics.getCardinality(q4), 0.00001);
         sail.shutDown();
     }
 
